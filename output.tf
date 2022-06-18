49477eee55fbc7b72e2efccdbf25798542adce3f
marieane22 <marieane@live.com>
2022-06-18 17:32:15 +0000

    update password

--- output.tf
+++ output.tf
@@ -6,14 +6,15 @@ output "db_name" {
   value = azurerm_mysql_server.wordpress.name
 }
 # Gives the DB host address
-#output "db_addr" {
-  #value = azurerm_mysql_server.wordpress.address
-#}
-# Gives the username for DB access
-output "administrator_login" {
-  value = azurerm_mysql_server.wordpress.username
+output "db_addr" {
+  value = azurerm_mysql_server.wordpress.name.address
 }
+# Gives the username for DB access
+output "administrator_login"{
+  value = azurerm_mysql_server.wordpress.login
+}
+
 # Gives the password for DB access
-output "dadministrator_login_password" {
+output "administrator_login_password"{
   value = azurerm_mysql_server.wordpress.password
 }
