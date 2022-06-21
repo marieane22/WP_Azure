0f84a1d2b0ac04584187d309f298c04dd05f9e8e
marieane22 <marieane@live.com>
2022-06-21 22:49:32 +0000

    updates

--- Main.tf
+++ Main.tf
@@ -27,10 +27,11 @@ resource "azurerm_lb_rule" "lbnatrule" {
   resource_group_name            = azurerm_resource_group.wordpress.name
   loadbalancer_id                = azurerm_lb.wordpress.id
   name                           = "http"
-  destination_port_range         = "80"
   direction                      = "Inbound"
   access                         = "Allow"
   protocol                       = "Tcp"
+  source_port_range              = "*"
+  destination_port_range         = "80"
   frontend_port                  = var.application_port
   backend_port                   = var.application_port
   backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
